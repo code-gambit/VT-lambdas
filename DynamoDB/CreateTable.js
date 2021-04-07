4321459e73462d411221bcac63ea1fcfe956916c
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2021-04-07 16:51:09 +0530

    Adding dynamodb create schema (#7)
    * Added dynamodb CreateTable script
    
    * Adde bug issue template
    
    Added new lambda issue template
    
    * Fixes #2: Added PR template (#3)
    
    * Added template for PR
    
    Fixes #2
    
    * Update pull_request_template.md
    
    * Update pull_request_template.md
    
    * Update:Adding dynamodb create schema
    
    * Updated keys configuration for AWS Console
    
    Co-authored-by: danishjamal104 <danishjamal.104@gmail.com>
    Co-authored-by: Ayush Tiwari <ayushtiwari5747462@gmail.com>

---
+++ DynamoDB/CreateTable.js
@@ -0,0 +1,70 @@
+var AWS = require("aws-sdk");
+
+/*Use this for AWS Console*/
+//AWS.config.loadFromPath('../keys.json');
+
+/*Use this for local setup*/
+AWS.config.update({
+  region: "local",
+  endpoint: "http://localhost:8000"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+var params = {
+    TableName : "V-Transfer",
+    KeySchema: [       
+        { AttributeName: "PK", KeyType: "HASH"},  //Partition key
+        { AttributeName: "SK", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "PK", AttributeType: "S" },
+        { AttributeName: "SK", AttributeType: "S" },
+        { AttributeName: "U_ID", AttributeType: "S" },
+        { AttributeName: "LS1_SK", AttributeType: "S"}
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    },
+    GlobalSecondaryIndexes: [{
+        IndexName: "FIND_FILE",
+        KeySchema: [
+            {
+                AttributeName: "U_ID",
+                KeyType: "HASH"
+            }
+        ],
+        Projection: {
+            ProjectionType: "ALL"
+        },
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+        }
+    }],
+    LocalSecondaryIndexes: [{
+        IndexName: "FILTER_BY_NAME",
+        KeySchema: [
+            {
+                AttributeName: "PK",
+                KeyType: "HASH"
+                },
+            {
+                AttributeName: "LS1_SK",
+                KeyType: "RANGE"
+                }
+        ],
+        Projection: {
+            ProjectionType: "ALL"
+        }
+    }]
+};
+
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
