dffc17469eea071413efd237445616c415f216aa
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-04-14 02:27:03 +0530

    Updated createUser lambda

--- lambdas/user_access_patterns/lambda_for_createUser/index.js
+++ lambdas/user_access_patterns/lambda_for_createUser/index.js
@@ -1,11 +1,13 @@
 const AWS = require("aws-sdk");
-const crypto = require("crypto");
-//AWS.config.loadFromPath("../../../keys.json");
+
+//AWS.config.loadFromPath("../../../keys.json")
+
 //Use the below code for local setup
 /*AWS.config.update({
   region: "local",
   endpoint: "http://localhost:8000",
 });*/
+
 const dynamo = new AWS.DynamoDB.DocumentClient();
 function response(statusCode, message) {
   return {
@@ -14,39 +16,25 @@ function response(statusCode, message) {
   };
 }
 
-function hmac_hash(secretKey, password) {
-  const hashingSecret = secretKey;
-  const plainText = password;
-  const hashedStr = crypto
-    .createHmac("sha256", hashingSecret)
-    .update(plainText)
-    .digest("hex");
-
-  return hashedStr;
-}
-
 module.exports.createUser = async (event) => {
   const reqBody = event.body;
   const user = {
-    name: reqBody.name,
-    PK: `USER#${reqBody.user_email}`,
+    PK: `USER#${reqBody.u_email}`,
     SK: `METADATA`,
-    thumbnail: reqBody.thumbnail,
-    password:`${hmac_hash(reqBody.user_email, reqBody.user_password)}`,
-    verified: true,
-    provider: reqBody.provider,
+    name: reqBody.u_name,
+    type: reqBody.u_type,
     storage_used: 0,
-    type: "default",
+    thumbnail: reqBody.u_thumbnail,
   };
 
   try{
-      await dynamo.put({
-        TableName:"V-Transfer",
-        Item: user
-      }).promise()
-      return response(201,`User ${reqBody.name} created successfully`)
-    }
-    catch(err){
-      return response(err.statusCode,err.message)
-    }
+    await dynamo.put({
+      TableName:"V-Transfer",
+      Item: user
+    }).promise()
+    return response(201,`User ${reqBody.name} created successfully`)
   }
+  catch(err){
+    return response(err.statusCode,err.message)
+  }
+}
