2ec97432b3c358e259ebd47bab8e28967916fcd8
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-04-26 20:36:29 +0530

    Added lambda for getHash and minor changes in uploadFile lambda

---
+++ lambdas/url_access_patterns/lambda_for_getHash/index.js
@@ -0,0 +1,50 @@
+const AWS = require("aws-sdk");
+
+//AWS.config.loadFromPath("../../../keys.json");
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+function response(statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: message,
+  };
+}
+
+exports.getHash = async (event) =>{
+    try{
+        var params={
+            TableName:"V-Transfer",
+            IndexName:"FIND_FILE_BY_URLID",
+            KeyConditionExpression: "GS1_PK= :pk",
+            ExpressionAttributeValues:{
+                ':pk':`${event.path.url_id}`,                
+            },                                    
+        }        
+        var url_data = await dynamo.query(params).promise()  
+        if(url_data.Items[0]==undefined) return response(404,"Provided URL is invalid"); 
+        url_data=url_data.Items[0];
+        if(url_data.visible===false)return response(403,"Provided URL is not accessible")
+        if(url_data.clicks_left<=0)return response(403,"Provided URL is not accessible")
+    }
+    catch(err){
+        return response(err.statusCode,err.message);
+    }
+    try{
+        await dynamo.update({
+            TableName: "V-Transfer",
+            Key: {
+                PK: `${url_data.PK}`,
+                SK: `${url_data.SK}`
+            },
+            UpdateExpression: "add clicks_left :val",
+            ExpressionAttributeValues: {
+                ":val": -1
+            }
+        }).promise();
+        return response(200,url_data.hash);
+    }
+    catch(err){
+        return response(err.statusCode,err.message);
+    }
+}
